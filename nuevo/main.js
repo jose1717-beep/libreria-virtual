5273440342c42c02f8fcfa258bd2a55c54c40cfd
jose1717-beep <tochuna1708@gmail.com>
2024-11-06 00:32:43 -0400

    Add files via upload

---
+++ nuevo/main.js
@@ -0,0 +1,542 @@
+const productos = 
+[
+    {
+        id: "libros-01",
+        titulo: " 01",
+        imagen: "bio1.png",
+        categoria: {
+            nombre: "Bienvenido a los libros mas recomendados",
+            id: "libros"
+        },
+        precio: 1000
+    },
+    {
+        id: "libros-02",
+        titulo: " 02",
+        imagen: "bio2.png",
+        categoria: {
+            nombre: "Bienvenido a los libros mas recomendados",
+            id: "libros"
+        },
+        precio: 1000
+    },
+    {
+        id: "libros-03",
+        titulo: " 02",
+        imagen: "bio3.png",
+        categoria: {
+            nombre: "Bienvenido a los libros mas recomendados",
+            id: "libros"
+        },
+        precio: 1000
+    },
+    {
+        id: "libros-04",
+        titulo: " 02",
+        imagen: "bio4.png",
+        categoria: {
+            nombre: "Bienvenido a los libros mas recomendados",
+            id: "libros"
+        },
+        precio: 1000
+    },
+    {
+        id: "libros-05",
+        titulo: " 02",
+        imagen: "bio5.png",
+        categoria: {
+            nombre: "Bienvenido a los libros mas recomendados",
+            id: "libros"
+        },
+        precio: 1000
+    },
+    {
+        id: "libros-06",
+        titulo: " 02",
+        imagen: "bio6.png",
+        categoria: {
+            nombre: "Bienvenido a los libros mas recomendados",
+            id: "libros"
+        },
+        precio: 1000
+    },
+    {
+        id: "libros-07",
+        titulo: " 02",
+        imagen: "ingles1.png",
+        categoria: {
+            nombre: "Bienvenido a los libros mas recomendados",
+            id: "libros"
+        },
+        precio: 1000
+    },
+    {
+        id: "libros-02",
+        titulo: " 02",
+        imagen: "ingles2.png",
+        categoria: {
+            nombre: "Bienvenido a los libros mas recomendados",
+            id: "libros"
+        },
+        precio: 1000
+    },
+    {
+        id: "libros-02",
+        titulo: " 02",
+        imagen: "ingles3.png",
+        categoria: {
+            nombre: "Bienvenido a los libros mas recomendados",
+            id: "libros"
+        },
+        precio: 1000
+    },
+    {
+        id: "libros-02",
+        titulo: " 02",
+        imagen: "ingles4.png",
+        categoria: {
+            nombre: "Bienvenido a los libros mas recomendados",
+            id: "libros"
+        },
+        precio: 1000
+    },
+    {
+        id: "libros-02",
+        titulo: " 02",
+        imagen: "ingles5.png",
+        categoria: {
+            nombre: "Bienvenido a los libros mas recomendados",
+            id: "libros"
+        },
+        precio: 1000
+    },
+    {
+        id: "libros-02",
+        titulo: " 02",
+        imagen: "ingles6.png",
+        categoria: {
+            nombre: "Bienvenido a los libros mas recomendados",
+            id: "libros"
+        },
+        precio: 1000
+    },
+    {
+        id: "libros-02",
+        titulo: " 02",
+        imagen: "soc1.png",
+        categoria: {
+            nombre: "Bienvenido a los libros mas recomendados",
+            id: "libros"
+        },
+        precio: 1000
+    },
+    {
+        id: "libros-02",
+        titulo: " 02",
+        imagen: "soc2.png",
+        categoria: {
+            nombre: "Bienvenido a los libros mas recomendados",
+            id: "libros"
+        },
+        precio: 1000
+    },
+    {
+        id: "libros-02",
+        titulo: " 02",
+        imagen: "soc3.png",
+        categoria: {
+            nombre: "Bienvenido a los libros mas recomendados",
+            id: "libros"
+        },
+        precio: 1000
+    },
+    {
+        id: "libros-02",
+        titulo: " 02",
+        imagen: "soc4.png",
+        categoria: {
+            nombre: "Bienvenido a los libros mas recomendados",
+            id: "libros"
+        },
+        precio: 1000
+    },
+    {
+        id: "libros-03",
+        titulo: " 03",
+        imagen: "soc5.png",
+        categoria: {
+            nombre: "Bienvenido a los libros mas recomendados",
+            id: "libros"
+        },
+        precio: 1000
+    },
+    {
+        id: "libros-04",
+        titulo: " 04",
+        imagen: "soc6.png",
+        categoria: {
+            nombre: "Bienvenido a los libros mas recomendados",
+            id: "libros"
+        },
+        precio: 1000
+    },
+    {
+        id: "libros-0",
+        titulo: " 05",
+        imagen: "lengua1.png",
+        categoria: {
+            nombre: "Bienvenido a los libros mas recomendados",
+            id: "libros"
+        },
+        precio: 1000
+    },
+    {
+        id: "libros-0",
+        titulo: " 05",
+        imagen: "lengua2.png",
+        categoria: {
+            nombre: "Bienvenido a los libros mas recomendados",
+            id: "libros"
+        },
+        precio: 1000
+    },
+    {
+        id: "libros-0",
+        titulo: " 05",
+        imagen: "lengua3.png",
+        categoria: {
+            nombre: "Bienvenido a los libros mas recomendados",
+            id: "libros"
+        },
+        precio: 1000
+    },
+    {
+        id: "libros-0",
+        titulo: " 05",
+        imagen: "lengua4.png",
+        categoria: {
+            nombre: "Bienvenido a los libros mas recomendados",
+            id: "libros"
+        },
+        precio: 1000
+    },
+    {
+        id: "libros-0",
+        titulo: " 05",
+        imagen: "lengua5.png",
+        categoria: {
+            nombre: "Bienvenido a los libros mas recomendados",
+            id: "libros"
+        },
+        precio: 1000
+    },
+    {
+        id: "libros-0",
+        titulo: " 05",
+        imagen: "lengua6.png",
+        categoria: {
+            nombre: "Bienvenido a los libros mas recomendados",
+            id: "libros"
+        },
+        precio: 1000
+    },
+    {
+        id: "libros-0",
+        titulo: " 05",
+        imagen: "quim.png",
+        categoria: {
+            nombre: "Bienvenido a los libros mas recomendados",
+            id: "libros"
+        },
+        precio: 1000
+    },
+    {
+        id: "libros-0",
+        titulo: " 05",
+        imagen: "quim1.png",
+        categoria: {
+            nombre: "Bienvenido a los libros mas recomendados",
+            id: "libros"
+        },
+        precio: 1000
+    },
+    {
+        id: "libros-0",
+        titulo: " 05",
+        imagen: "quim3.png",
+        categoria: {
+            nombre: "Bienvenido a los libros mas recomendados",
+            id: "libros"
+        },
+        precio: 1000
+    },
+    {
+        id: "libros-0",
+        titulo: " 05",
+        imagen: "quim4.png",
+        categoria: {
+            nombre: "Bienvenido a los libros mas recomendados",
+            id: "libros"
+        },
+        precio: 1000
+    },
+    {
+        id: "libros-0",
+        titulo: " 05",
+        imagen: ".png",
+        categoria: {
+            nombre: "Bienvenido a los libros mas recomendados",
+            id: "libros"
+        },
+        precio: 1000
+    },
+    /////////////////////////////////////////////////////////////////////
+    {
+        id: "camiseta-01",
+        titulo: "Camiseta 01",
+        imagen: "",
+        categoria: {
+            nombre: "",
+            id: "cuadernos"
+        },
+        precio: 1000
+    },
+    {
+        id: "camiseta-02",
+        titulo: "Camiseta 02",
+        imagen: "",
+        categoria: {
+            nombre: "",
+            id: ""
+        },
+        precio: 1000
+    },
+    {
+        id: "camiseta-03",
+        titulo: "Camiseta 03",
+        imagen: "",
+        categoria: {
+            nombre: "",
+            id: ""
+        },
+        precio: 1000
+    },
+    {
+        id: "camiseta-04",
+        titulo: "Camiseta 04",
+        imagen: "",
+        categoria: {
+            nombre: "",
+            id: ""
+        },
+        precio: 1000
+    },
+    {
+        id: "camiseta-05",
+        titulo: "Camiseta 05",
+        imagen: "",
+        categoria: {
+            nombre: "",
+            id: ""
+        },
+        precio: 1000
+    },
+    {
+        id: "camiseta-06",
+        titulo: "Camiseta 06",
+        imagen: "",
+        categoria: {
+            nombre: "",
+            id: ""
+        },
+        precio: 1000
+    },
+    {
+        id: "camiseta-07",
+        titulo: "Camiseta 07",
+        imagen: "",
+        categoria: {
+            nombre: "",
+            id: ""
+        },
+        precio: 1000
+    },
+    {
+        id: "camiseta-08",
+        titulo: "Camiseta 08",
+        imagen: "",
+        categoria: {
+            nombre: "",
+            id: ""
+        },
+        precio: 1000
+    },
+    /////////////////////////////////////////////////////////////////////
+    {
+        id: "pantalon-01",
+        titulo: "Pantalón 01",
+        imagen: "estuche.jpg",
+        categoria: {
+            nombre: "",
+            id: "otros"
+        },
+        precio: 1000
+    },
+    {
+        id: "pantalon-02",
+        titulo: "Pantalón 02",
+        imagen: "",
+        categoria: {
+            nombre: "",
+            id: ""
+        },
+        precio: 1000
+    },
+    {
+        id: "pantalon-03",
+        titulo: "Pantalón 03",
+        imagen: "",
+        categoria: {
+            nombre: "",
+            id: ""
+        },
+        precio: 1000
+    },
+    {
+        id: "pantalon-04",
+        titulo: "Pantalón 04",
+        imagen: "",
+        categoria: {
+            nombre: "",
+            id: ""
+        },
+        precio: 1000
+    },
+    {
+        id: "pantalon-05",
+        titulo: "Pantalón 05",
+        imagen: "",
+        categoria: {
+            nombre: "",
+            id: ""
+        },
+        precio: 1000
+    }
+];
+
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+let botonesAgregar = document.querySelectorAll(".producto-agregar");
+const numerito = document.querySelector("#numerito");
+
+
+botonesCategorias.forEach(boton => boton.addEventListener("click", () => {
+    aside.classList.remove("aside-visible");
+}))
+
+
+function cargarProductos(productosElegidos) {
+
+    contenedorProductos.innerHTML = "";
+
+    productosElegidos.forEach(producto => {
+
+        const div = document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = `
+            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+            <div class="producto-detalles">
+                <h3 class="producto-titulo">${producto.titulo}</h3>
+                <p class="producto-precio">$${producto.precio}</p>
+                <button class="producto-agregar" id="${producto.id}">Agregar</button>
+            </div>
+        `;
+
+        contenedorProductos.append(div);
+    })
+
+    actualizarBotonesAgregar();
+}
+
+
+botonesCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+
+        botonesCategorias.forEach(boton => boton.classList.remove("active"));
+        e.currentTarget.classList.add("active");
+
+        if (e.currentTarget.id != "todos") {
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+            cargarProductos(productosBoton);
+        } else {
+            tituloPrincipal.innerText = "Todos los productos";
+            cargarProductos(productos);
+        }
+
+    })
+});
+
+function actualizarBotonesAgregar() {
+    botonesAgregar = document.querySelectorAll(".producto-agregar");
+
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
+    });
+}
+
+let productosEnCarrito;
+
+let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+
+if (productosEnCarritoLS) {
+    productosEnCarrito = JSON.parse(productosEnCarritoLS);
+    actualizarNumerito();
+} else {
+    productosEnCarrito = [];
+}
+
+function agregarAlCarrito(e) {
+
+    Toastify({
+        text: "Producto agregado",
+        duration: 3000,
+        close: true,
+        gravity: "top",
+        position: "right",
+        stopOnFocus: true,
+        style: {
+          background: "linear-gradient(to right, #4b33a8, #785ce9)",
+          borderRadius: "2rem",
+          textTransform: "uppercase",
+          fontSize: ".75rem"
+        },
+        offset: {
+            x: '1.5rem',
+            y: '1.5rem'
+          },
+        onClick: function(){} 
+      }).showToast();
+
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
+
+    if(productosEnCarrito.some(producto => producto.id === idBoton)) {
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito[index].cantidad++;
+    } else {
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado);
+    }
+
+    actualizarNumerito();
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+}
+
+function actualizarNumerito() {
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerText = nuevoNumerito;
+}
